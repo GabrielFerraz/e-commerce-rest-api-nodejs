39f9f862d17da35268589fb8068d9a6ad9fe7246
Gabriel <gabrielmaiaferraz@gmail.com>
2017-11-19 17:07:56 +1300

    basic controller

---
+++ api/controllers/restController.js
@@ -0,0 +1,45 @@
+var mongoose = require('mongoose'),
+    Product = mongoose.model('Products');
+
+export.list_all_products = function(req, res) {
+  Product.find({}, function(err, product) {
+    if (err)
+      res.send(err);
+    res.json(product);
+  });
+};
+
+export.create_a_product = function(req, res) {
+  var new_product = new Product(req.body);
+  new_product.save(function(err, product) {
+    if (err)
+      res.send(err);
+    res.json(product);
+  });
+};
+
+export.read_a_product = function(req, res) {
+  Product.findById(req.params.productId, function(err, task) {
+    if (err)
+      res.send(err);
+    res.json(product);
+  });
+};
+
+export.update_a_product = function(req, res) {
+  Product.findOneAndUpdate({_id: req.params.productId}, req.body, {new: true}, function(err, product) {
+    if (err)
+      res.send(err);
+    res.json(product);
+  });
+};
+
+export.delete_a_product = function(req, res) {
+  Product.remove({
+    _id: req.params.productId
+  }, function(err, product) {
+    if (err)
+      res.send(err);
+    res.json({ message: 'Product successfully deleted' });
+  });
+};
